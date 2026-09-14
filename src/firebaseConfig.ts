8d344c26817d00a825bf84239cdc9f954f1f964a
NayPyon <nayakaingram@gmail.com>
2026-09-14 20:59:45 +0800

    feat: update firebase configuration to use environment variables and add example .env file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -10,14 +10,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkl7YQ_7yKAnfnw8EfO-8o8cUMKpzO8_M",
-  authDomain: "eco-point-2230e.firebaseapp.com",
-  databaseURL:
-    "https://eco-point-2230e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "eco-point-2230e",
-  storageBucket: "eco-point-2230e.firebasestorage.app",
-  messagingSenderId: "978442311403",
-  appId: "1:978442311403:web:f6b88dbcf3226224dd50ae",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp;
